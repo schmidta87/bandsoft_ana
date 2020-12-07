89c9875db81f1a77b19be9f212d33a48830cfb1b
Efrain Segarra <efrainsegarra@efrains-mbp.lan>
2020-12-07 14:51:53 -0500

    * Moved cuts to header file * Removed initializing TTree in final inclusive since it's just a clone   we don't need to

--- final_skim/final_inclusive.cpp
+++ final_skim/final_inclusive.cpp
@@ -12,6 +12,7 @@
 #include "TChain.h"
 
 #include "constants.h"
+#include "kinematic_cuts.h"
 #include "bandhit.h"
 #include "clashit.h"
 #include "taghit.h"
@@ -23,40 +24,24 @@ using namespace std;
 int slc[6][5] = {{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,6,6,2},{3,7,5,5,0},{3,7,6,6,2}};
 
 int main(int argc, char ** argv){
-	// Set style
-	gStyle->SetOptFit(1);
-
-	if (argc < 3){
+	if (argc < 2){
 		cerr << "Wrong number of arguments. Instead use\n"
 			<< "\t./code [outfile.root] [inputDatafiles]\n";
 		return -1;
 	}
 
-	// Electron cuts
-	const int PID = 11;
-	const int charge = -1;
-	const double EoP_min = 0.17;
-	const double EoP_max = 0.3;
-	const double Epcal_min = 0.07;
-	const double V_min = 15;
-	const double W_min = 15;
-	const double vtx_min = -8;
-	const double vtx_max =  3;
-	const double pE_min = 3;
-	const double pE_max = 10.6;
-	const double Q2_min = 2;
-	const double Q2_max = 10;
-	const double W2_min = 2*2;
-	TCut ePID 	= Form("eHit->getPID() == %i",PID);
-	TCut eCharge 	= Form("eHit->getCharge() == %i",charge); 
-	TCut eEoP	= Form("eHit->getEoP() > %f && eHit->getEoP() < %f",EoP_min,EoP_max);
-	TCut eEpcal	= Form("eHit->getEpcal() > %f",Epcal_min);
-	TCut eVW	= Form("eHit->getV() > %f && eHit->getW() > %f",V_min,W_min);
-	TCut eVtx	= Form("eHit->getVtz() > %f && eHit->getVtz() < %f",vtx_min,vtx_max);
-	TCut eMom	= Form("eHit->getMomentum() > %f && eHit->getMomentum() < %f",pE_min,pE_max);
-	TCut eQ2	= Form("eHit->getQ2() > %f && eHit->getQ2() < %f",Q2_min,Q2_max);
-	TCut eW		= Form("eHit->getW2() > %f",W2_min);
+	// Conditions for a final accepted event
+	TCut ePID 	= Form("eHit->getPID() == %i",					ECUT_PID);
+	TCut eCharge 	= Form("eHit->getCharge() == %i",				ECUT_charge); 
+	TCut eEoP	= Form("eHit->getEoP() > %f && eHit->getEoP() < %f",		ECUT_EoP_min,		ECUT_EoP_max);
+	TCut eEpcal	= Form("eHit->getEpcal() > %f",					ECUT_Epcal_min);
+	TCut eVW	= Form("eHit->getV() > %f && eHit->getW() > %f",		ECUT_V_min,		ECUT_W_min);
+	TCut eVtx	= Form("eHit->getVtz() > %f && eHit->getVtz() < %f",		ECUT_vtx_min,		ECUT_vtx_max);
+	TCut eMom	= Form("eHit->getMomentum() > %f && eHit->getMomentum() < %f",	ECUT_pE_min,		ECUT_pE_max);
+	TCut eQ2	= Form("eHit->getQ2() > %f && eHit->getQ2() < %f",		ECUT_Q2_min,		ECUT_Q2_max);
+	TCut eW		= Form("eHit->getW2() > %f",					ECUT_W2_min);
 	TCut inclusive	= ePID && eCharge && eEoP && eEpcal && eVW && eVtx && eMom && eQ2 && eW;
+	//TString cut = Form("%s",inclusive.GetTitle());
 	TString cut = Form("%s",inclusive.GetTitle());
 
 	// Load input files
@@ -65,43 +50,20 @@ int main(int argc, char ** argv){
 		cout << "Adding file " << argv[i] << endl;
 		inTree->Add(argv[i]);
 	}
-	// Initialize the input branches
-	int Runno		= 0;
-	double Ebeam		= 0;
-	double gated_charge	= 0;
-	double livetime		= 0;
-	double starttime	= 0;
-	double current		= 0;
-	clashit* eHit = new clashit;
-	inTree->SetBranchAddress("Runno"		,&Runno			);
-	inTree->SetBranchAddress("Ebeam"		,&Ebeam			);
-	inTree->SetBranchAddress("gated_charge"		,&gated_charge		);
-	inTree->SetBranchAddress("livetime"		,&livetime		);
-	inTree->SetBranchAddress("starttime"		,&starttime		);
-	inTree->SetBranchAddress("current"		,&current		);
-	// 	Electron branches:
-	inTree->SetBranchAddress("eHit"			,&eHit			);
 
-	// Output rootfile
+	// Create an output rootfile
 	TFile * outFile = new TFile(argv[1],"RECREATE");
 	inTree->LoadTree(0);
 	TTree * outTree = inTree->CloneTree(0);
 
+	// Create the good event list from our cuts defined above
 	inTree->Draw(">>goodEvents",cut);
 	TEventList * goodEvents = (TEventList*) gDirectory->Get("goodEvents");
 	int nEvents = goodEvents->GetN();
 
+	// Loop over all the good events and write the output tree
 	for( int ev = 0 ; ev < nEvents ; ev++ ){
 		if( ev % 100000 == 0 ) cout << "\ton event " << ev << "\n";
-
-		// Clear all branches before getting the entry from tree
-		Runno 		= 0;
-		Ebeam		= 0;
-		gated_charge	= 0;
-		livetime	= 0;
-		starttime 	= 0;
-		current		= 0;
-		eHit->Clear();
 
 		int entry = goodEvents->GetEntry(ev);
 		inTree->GetEntry(entry);
@@ -111,6 +73,7 @@ int main(int argc, char ** argv){
 	} // end loop over events
 
 
+	// Write and close
 	outTree->Write();
 	outFile->Close();
 	return 0;
---
+++ include/kinematic_cuts.h
@@ -0,0 +1,19 @@
+#ifndef __KINEMATIC_CUTS_H__
+#define __KINEMATIC_CUTS_H__
+
+// Electron cuts
+const int 	ECUT_PID = 11;
+const int 	ECUT_charge = -1;
+const double 	ECUT_EoP_min = 0.17;
+const double 	ECUT_EoP_max = 0.3;
+const double 	ECUT_Epcal_min = 0.07;
+const double 	ECUT_V_min = 15;
+const double 	ECUT_W_min = 15;
+const double 	ECUT_vtx_min = -8;
+const double 	ECUT_vtx_max =  3;
+const double 	ECUT_pE_min = 3;
+const double 	ECUT_pE_max = 10.6;
+const double 	ECUT_Q2_min = 2;
+const double 	ECUT_Q2_max = 10;
+const double 	ECUT_W2_min = 2*2;
+#endif
