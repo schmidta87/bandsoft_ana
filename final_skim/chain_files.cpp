44814735256756df09f042cba6b13e0c4cd99e52
Efrain Segarra <segarrae@ifarm1802.jlab.org>
2021-09-24 10:42:55 -0400

    * added histograms to final tagged skim * added opts for cuts on smeared for MC in final skim tagged * updated chain for sim,data,mix

--- final_skim/chain_files.cpp
+++ final_skim/chain_files.cpp
@@ -11,22 +11,28 @@
 using namespace std;
 int main(int argc, char ** argv){
 
-	if (argc < 2){
+	if (argc < 3){
 		cerr << "Wrong number of arguments. Instead use\n"
-			<< "\t./code [inputDatafiles]\n";
+			<< "\t./code [opt 0 (MC) 1 (dat) 2 (bac) ] [inputDatafiles]\n";
 		return -1;
 	}
-
-	TFile * output = new TFile("CHAINED.root","RECREATE");
+	int MC_DATA_OPT = atoi(argv[1]);
+	TFile * output = NULL;
+	if( MC_DATA_OPT == 0 ) output = new TFile("SIM-CHAINED.root","RECREATE");
+	if( MC_DATA_OPT == 1 ) output = new TFile("DAT-CHAINED.root","RECREATE");
+	if( MC_DATA_OPT == 2 ) output = new TFile("MIX-CHAINED.root","RECREATE");
+	
 	TChain * tagged = new TChain("tagged");
 	TH1D * hToF_full = new TH1D("hToF_full","hToF_full",8000,-100,300);
+	TH1D * hThetaNQ = new TH1D("hThetaNQ","hThetaNQ",8000,140,180);
+	TH1D * hThetaN 	= new TH1D("hThetaN","hThetaN",8000,140,180);
+	TH1D * hPe = new TH1D("hPe","hPe",4000,3,7);
 	
 
-	
 	double total_normalization_x = 0;
 	double total_normalization_y = 0;
 	double total_normalization_z = 0;
-	for( int fi = 1 ; fi < argc ; fi++){
+	for( int fi = 2 ; fi < argc ; fi++){
 		cout << argv[fi] << "\n";
 		// Add file to chain:
 		tagged->Add(argv[fi]);
@@ -35,15 +41,27 @@ int main(int argc, char ** argv){
 		TFile * thisFile = new TFile(argv[fi]);
 
 		TH1D * thisToF = (TH1D*)thisFile->Get("hToF_full");
+		TH1D * thisThetaNQ = (TH1D*)thisFile->Get("hThetaNQ");
+		TH1D * thisThetaN = (TH1D*)thisFile->Get("hThetaN");
+		TH1D * thisPe = (TH1D*)thisFile->Get("hPe");
+		
 		hToF_full->Add( thisToF , 1 );
+		hThetaNQ->Add( thisThetaNQ , 1);
+		hThetaN->Add( thisThetaN, 1 );
+		hPe->Add( thisPe , 1 );
+		
+		if( MC_DATA_OPT > 0 ){
+			TVector3 * thisNorm = (TVector3*)thisFile->Get("bacnorm");
+			total_normalization_x	+=	thisNorm->X();
+			total_normalization_y	+=	thisNorm->Y();
+			total_normalization_z	+=	thisNorm->Z();
+			delete thisNorm;
+		}	
 
-		TVector3 * thisNorm = (TVector3*)thisFile->Get("bacnorm");
-		total_normalization_x	+=	thisNorm->X();
-		total_normalization_y	+=	thisNorm->Y();
-		total_normalization_z	+=	thisNorm->Z();
-
-		delete thisNorm;
 		delete thisToF;
+		delete thisThetaNQ;
+		delete thisThetaN;
+		delete thisPe;
 		thisFile->Close();
 	}
 
@@ -56,6 +74,9 @@ int main(int argc, char ** argv){
 	bacnorm->Write("bacnorm");
 
 	hToF_full->Write("hToF_full");
+	hThetaNQ->Write("hThetaNQ");
+	hThetaN->Write("hThetaN");
+	hPe->Write("hPe");
 
 	output->Write();
 	output->Close();
--- final_skim/final_tagged_newnorm.cpp
+++ final_skim/final_tagged_newnorm.cpp
@@ -36,30 +36,46 @@ int main(int argc, char ** argv){
 		return -1;
 	}
 	int MC_DATA_OPT = atoi(argv[2]);
+	TCut ePID 		= "";
+        TCut eCharge 		= "";
+        TCut eEoP		= "";
+        TCut eEpcal		= "";
+        TCut eVW		= "";
+        TCut eVtx		= "";
+        TCut eMom		= "";
+        TCut eQ2		= "";
+        TCut eW			= "";
 
 	// Conditions for a final accepted event electron
-	TCut ePID 	= Form("eHit->getPID() == %i",					ECUT_PID);
-	TCut eCharge 	= Form("eHit->getCharge() == %i",				ECUT_charge); 
-	TCut eEoP	= Form("eHit->getEoP() > %f && eHit->getEoP() < %f",		ECUT_EoP_min,		ECUT_EoP_max);
-	TCut eEpcal	= Form("eHit->getEpcal() > %f",					ECUT_Epcal_min);
-	TCut eVW	= Form("eHit->getV() > %f && eHit->getW() > %f",		ECUT_V_min,		ECUT_W_min);
-	TCut eVtx	= Form("eHit->getVtz() > %f && eHit->getVtz() < %f",		ECUT_vtx_min,		ECUT_vtx_max);
-	TCut eMom	= Form("eHit->getMomentum() > %f && eHit->getMomentum() < %f",	ECUT_pE_min,		ECUT_pE_max);
-	TCut eQ2	= Form("eHit->getQ2() > %f && eHit->getQ2() < %f",		ECUT_Q2_min,		ECUT_Q2_max);
-	TCut eW		= Form("eHit->getW2() > %f",					ECUT_W2_min);
+	if( MC_DATA_OPT == 1 || MC_DATA_OPT == 2 ){
+		ePID 	= Form("eHit->getPID() == %i",					ECUT_PID);
+		eCharge = Form("eHit->getCharge() == %i",				ECUT_charge); 
+		eEoP	= Form("eHit->getEoP() > %f && eHit->getEoP() < %f",		ECUT_EoP_min,		ECUT_EoP_max);
+		eEpcal	= Form("eHit->getEpcal() > %f",					ECUT_Epcal_min);
+		eVW	= Form("eHit->getV() > %f && eHit->getW() > %f",		ECUT_V_min,		ECUT_W_min);
+		eVtx	= Form("eHit->getVtz() > %f && eHit->getVtz() < %f",		ECUT_vtx_min,		ECUT_vtx_max);
+		eMom	= Form("eHit->getMomentum() > %f && eHit->getMomentum() < %f",	ECUT_pE_min,		ECUT_pE_max);
+		eQ2	= Form("eHit->getQ2() > %f && eHit->getQ2() < %f",		ECUT_Q2_min,		ECUT_Q2_max);
+		eW	= Form("eHit->getW2() > %f",					ECUT_W2_min);
+	}
+	if( MC_DATA_OPT == 0 ){
+		ePID 	= Form("eHit_smeared->getPID() == %i",						ECUT_PID);
+		eCharge = Form("eHit_smeared->getCharge() == %i",					ECUT_charge); 
+		eEoP	= Form("eHit_smeared->getEoP() > %f && eHit_smeared->getEoP() < %f",		ECUT_EoP_min,		ECUT_EoP_max);
+		eEpcal	= Form("eHit_smeared->getEpcal() > %f",						ECUT_Epcal_min);
+		eVW	= Form("eHit_smeared->getV() > %f && eHit_smeared->getW() > %f",		ECUT_V_min,		ECUT_W_min);
+		eVtx	= Form("eHit_smeared->getVtz() > %f && eHit_smeared->getVtz() < %f",		ECUT_vtx_min,		ECUT_vtx_max);
+		eMom	= Form("eHit_smeared->getMomentum() > %f && eHit_smeared->getMomentum() < %f",	ECUT_pE_min,		ECUT_pE_max);
+		eQ2	= Form("eHit_smeared->getQ2() > %f && eHit_smeared->getQ2() < %f",		ECUT_Q2_min,		ECUT_Q2_max);
+		eW	= Form("eHit_smeared->getW2() > %f",						ECUT_W2_min);
+	}
 	TCut inclusive	= ePID && eCharge && eEoP && eEpcal && eVW && eVtx && eMom && eQ2 && eW;
 
 	// Conditions for a final accepted event neutron - signal or background
 	TCut nGood	= Form("goodneutron == %i",					NCUT_goodneutron);
 	TCut nLeadIdx	= Form("nleadindex != %i",					NCUT_leadindex);
-	TCut nEdep;
-		// data or background:
-	if( MC_DATA_OPT == 0 || MC_DATA_OPT == 2)
-		nEdep	= Form("nHits[nleadindex]->getEdep() > %f",			NCUT_Edep);
-		// sim:
-	else if( MC_DATA_OPT == 1)
-		nEdep	= Form("nHits[nleadindex]->getEdep() > %f",			NCUT_Edep);
-	TCut nThetaNQ	= Form("tag[nleadindex]->getThetaNQ() > %f && tag[nleadindex]->getThetaNQ() < %f",NCUT_THETANQ_min,NCUT_THETANQ_max);
+	TCut nEdep	= Form("nHits[nleadindex]->getEdep() > %f",			NCUT_Edep);
+	TCut nThetaNQ 		= "";
 		// make a fiducial cut around the edge of BAND:
 	TCut fiducial1	= Form("!(nHits[nleadindex]->getSector()==1 && (nHits[nleadindex]->getX() >  70 || nHits[nleadindex]->getX() < -70))");
 	TCut fiducial2	= Form("!(nHits[nleadindex]->getSector()==2 && (nHits[nleadindex]->getX() >  90 || nHits[nleadindex]->getX() < -90))");
@@ -67,7 +83,15 @@ int main(int argc, char ** argv){
 	TCut fiducial4	= Form("!(nHits[nleadindex]->getSector()==4 && (nHits[nleadindex]->getX() < -90 || nHits[nleadindex]->getX() > -60))");
 	TCut fiducial5	= Form("!(nHits[nleadindex]->getSector()==5 && (nHits[nleadindex]->getX() >  90 || nHits[nleadindex]->getX() < -90))");
 		// to this fiducial, implement a ThetaN cut for beam pipe issue:
-	TCut fiducialTheta	= Form("tag[nleadindex]->getMomentumN().Theta() < 168.5*TMath::Pi()/180.");
+	TCut fiducialTheta 	= "";
+	if( MC_DATA_OPT == 1 || MC_DATA_OPT == 2 ){
+		fiducialTheta	= Form("tag[nleadindex]->getMomentumN().Theta() < 168.5*TMath::Pi()/180.");
+		nThetaNQ	= Form("tag[nleadindex]->getThetaNQ() > %f && tag[nleadindex]->getThetaNQ() < %f",NCUT_THETANQ_min,NCUT_THETANQ_max);
+	}
+	if( MC_DATA_OPT == 0 ){
+		fiducialTheta	= Form("tag_smeared[nleadindex]->getMomentumN().Theta() < 168.5*TMath::Pi()/180.");
+		nThetaNQ	= Form("tag_smeared[nleadindex]->getThetaNQ() > %f && tag_smeared[nleadindex]->getThetaNQ() < %f",NCUT_THETANQ_min,NCUT_THETANQ_max);
+	}
 	TCut fiducial = fiducial1 && fiducial2 && fiducial3 && fiducial4 && fiducial5 && fiducialTheta;
 
 	// Bad bars for v3.1, consistent across run periods:
@@ -82,10 +106,22 @@ int main(int argc, char ** argv){
 
 	// Conditions for a final accepted event neutron in signal region
 	TCut nToF	= Form("nHits[nleadindex]->getTof() > %f && nHits[nleadindex]->getTof() < %f"	,NCUT_Tof_min, NCUT_Tof_max);
-	TCut nPn	= Form("tag[nleadindex]->getMomentumN().Mag() > %f && tag[nleadindex]->getMomentumN().Mag() < %f", NCUT_Pn_min, NCUT_Pn_max );
-	TCut nPnNaN	= Form("tag[nleadindex]->getMomentumN().Mag() == tag[nleadindex]->getMomentumN().Mag()");
-	TCut nWp	= Form("tag[nleadindex]->getWp() > %f && tag[nleadindex]->getWp() < %f",	NCUT_Wp_min,	NCUT_Wp_max);
-	TCut nAs	= Form("tag[nleadindex]->getAs() > %f && tag[nleadindex]->getAs() < %f",	NCUT_As_min,	NCUT_As_max);
+	TCut nPn	= "";
+	TCut nPnNaN	= "";
+	TCut nWp	= "";
+	TCut nAs	= "";
+	if( MC_DATA_OPT == 1 || MC_DATA_OPT == 2 ){
+		nPn	= Form("tag[nleadindex]->getMomentumN().Mag() > %f && tag[nleadindex]->getMomentumN().Mag() < %f", NCUT_Pn_min, NCUT_Pn_max );
+		nPnNaN	= Form("tag[nleadindex]->getMomentumN().Mag() == tag[nleadindex]->getMomentumN().Mag()");
+		nWp	= Form("tag[nleadindex]->getWp() > %f && tag[nleadindex]->getWp() < %f",	NCUT_Wp_min,	NCUT_Wp_max);
+		nAs	= Form("tag[nleadindex]->getAs() > %f && tag[nleadindex]->getAs() < %f",	NCUT_As_min,	NCUT_As_max);
+	}
+	if( MC_DATA_OPT == 0 ){
+		nPn	= Form("tag_smeared[nleadindex]->getMomentumN().Mag() > %f && tag_smeared[nleadindex]->getMomentumN().Mag() < %f", NCUT_Pn_min, NCUT_Pn_max );
+		nPnNaN	= Form("tag_smeared[nleadindex]->getMomentumN().Mag() == tag_smeared[nleadindex]->getMomentumN().Mag()");
+		nWp	= Form("tag_smeared[nleadindex]->getWp() > %f && tag_smeared[nleadindex]->getWp() < %f",	NCUT_Wp_min,	NCUT_Wp_max);
+		nAs	= Form("tag_smeared[nleadindex]->getAs() > %f && tag_smeared[nleadindex]->getAs() < %f",	NCUT_As_min,	NCUT_As_max);
+	}
 	TCut tagged_signal = tagged && nToF && nPn && nPnNaN && nWp && nAs;
 	
 	// Final TCut:
@@ -107,7 +143,10 @@ int main(int argc, char ** argv){
 	// get background normalization level for this neutron PID
 	TH1D * hToF_bac = NULL;
 	TH1D * hToF_far = NULL;
-	TH1D * hToF_full = NULL;
+	TH1D * hToF_full = new TH1D("hToF_full","hToF_full",8000,-100,300);
+	TH1D * hThetaNQ = new TH1D("hThetaNQ","hThetaNQ",8000,140,180);
+	TH1D * hThetaN 	= new TH1D("hThetaN","hThetaN",8000,140,180);
+	TH1D * hPe = new TH1D("hPe","hPe",4000,3,7);
 	TVector3 bacnorm;
 	if( MC_DATA_OPT == 1 ){ // if this is data file
 		
@@ -115,7 +154,6 @@ int main(int argc, char ** argv){
 		// and rescale it by the region sizes of Signal and Background.
 		hToF_bac = new TH1D("hToF_bac","hToF_bac",8000,-100,300);
 		hToF_far = new TH1D("hToF_far","hToF_far",8000,-100,300);
-		hToF_full = new TH1D("hToF_full","hToF_full",8000,-100,300);
 		TCut background_time = Form("nHits[nleadindex]->getTof() > %f && nHits[nleadindex]->getTof() < %f",
 						NCUT_BACK_Tof_min , NCUT_BACK_Tof_max );
 		TCut far_time = Form("nHits[nleadindex]->getTof() > %f && nHits[nleadindex]->getTof() < %f",
@@ -124,6 +162,10 @@ int main(int argc, char ** argv){
 		inTree->Draw("nHits[nleadindex]->getTof()  >> hToF_bac",tagged && background_time );
 		inTree->Draw("nHits[nleadindex]->getTof()  >> hToF_far",tagged && far_time );
 		inTree->Draw("nHits[nleadindex]->getTof()  >> hToF_full",tagged );
+
+		inTree->Draw("tag[nleadindex]->getThetaNQ() * 180./TMath::Pi() >> hThetaNQ",tagged && background_time );
+		inTree->Draw("tag[nleadindex]->getMomentumN().Theta() * 180./TMath::Pi() >> hThetaN",tagged && background_time );
+		inTree->Draw("eHit->getMomentum() >> hPe", tagged && background_time);
 
 		// Now we need to rescale this by the amount of bunches in the signal region:
 		double nSignalBunches 	= (NCUT_Tof_max - NCUT_Tof_min)/BEAM_BUNCH;
@@ -150,16 +192,24 @@ int main(int argc, char ** argv){
 	
 		//bacnorm.SetXYZ(background_counts,0,0);
 	}
-
 	if( MC_DATA_OPT == 2 ){ // if this is mixed file for background
 		// Grab the number of mixed events we have created
 		double N_mixed		= inTree->GetEntries(); // N_mixed is different from the correct normalization since
 								// now we have fiducial and bad bar cuts.
-		hToF_full = new TH1D("hToF_full","hToF_full",8000,-100,300);
 		inTree->Draw("nHits[nleadindex]->getTof()  >> hToF_full",tagged );
+		inTree->Draw("tag[nleadindex]->getThetaNQ() * 180./TMath::Pi() >> hThetaNQ",tagged );
+		inTree->Draw("tag[nleadindex]->getMomentumN().Theta() * 180./TMath::Pi() >> hThetaN",tagged );
+		inTree->Draw("eHit->getMomentum() >> hPe", tagged );
 		N_mixed = hToF_full->Integral();
 		
 		bacnorm.SetXYZ( N_mixed , 0 , 0 );
+
+	}
+	if( MC_DATA_OPT == 0 ){ // if this is a simulation file
+		inTree->Draw("nHits[nleadindex]->getTof()  >> hToF_full",tagged );
+		inTree->Draw("tag_smeared[nleadindex]->getThetaNQ() * 180./TMath::Pi() >> hThetaNQ",tagged );
+		inTree->Draw("tag_smeared[nleadindex]->getMomentumN().Theta() * 180./TMath::Pi() >> hThetaN",tagged );
+		inTree->Draw("eHit_smeared->getMomentum() >> hPe", tagged );
 
 	}
 
@@ -183,13 +233,17 @@ int main(int argc, char ** argv){
 	cout << "writing stuff to file:\n";
 	// Write and close
 	outFile->cd();
+	hToF_full->Write();
+	hThetaNQ->Write();
+	hThetaN->Write();
+	hPe->Write();
 	if( MC_DATA_OPT == 1 ){ // if this is data file
 		hToF_bac->Write();
 		hToF_far->Write();
-		hToF_full->Write();
 	}
 	if( MC_DATA_OPT == 2 ){
-		hToF_full->Write();
+	}
+	if( MC_DATA_OPT == 0 ){
 	}
 	outTree->Write();
 	bacnorm.Write("bacnorm");	
